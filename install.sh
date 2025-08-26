c55440b5240b98e5503be3f08b9f0526506c68d7
Jinner Rodriguez <djinner@gmail.com>
2025-08-26 15:49:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,15 +13,3 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo usermod -aG docker $USER # Agrega usuario al grupo Docker
-sudo groupadd docker # Crea el grupo si no está creado
-getent group docker
-newgrp docker # Reinicia la sesion
-ls -l /var/run/docker.sock # Verifica los permisos del socket de Docker
-
-# Si los permisos no son correctos o el grupo no es docker, puedes corregirlos temporalmente con:
-# sudo chown root:docker /var/run/docker.sock
-# sudo chmod 660 /var/run/docker.sock
-
-sudo systemctl restart docker
